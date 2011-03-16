9f4c62e39557454ffd77914811cba84d73392003
bernd <B.Katzmarski@gmx.de>
2011-03-16 15:25:34 +0100

    refactoring

--- lib/evasion-db/commands.rb
+++ lib/evasion-db/commands.rb
@@ -2,22 +2,31 @@ module FIDIUS
   module EvasionDB
     module Commands
 
-  def self.set_local_ip(ip)
-    $prelude_event_fetcher.local_ip = ip
-  end
+      def self.set_local_ip(ip)
+        $prelude_event_fetcher.local_ip = ip
+      end
 
-  def self.db_connect(config_file)
-    $prelude_event_fetcher.connect_db(config_file)
-  end
+      def self.db_connect(config_file)
+        $prelude_event_fetcher.connect_db(config_file)
+      end
 
-  def self.start_attack
-    $prelude_event_fetcher.attack_started
-  end
+      def self.start_attack
+        $prelude_event_fetcher.attack_started
+      end
 
-  def self.fetch_events
-    $prelude_event_fetcher.get_events
-  end
+      def self.fetch_events
+        $prelude_event_fetcher.get_events
+      end
 
-    end
-  end# module Commands
+      def self.log_packet(caused_by,data,socket)
+        begin
+          Packet.create(:exploit=>caused_by, :payload=>data,:src_addr=>socket.localhost,:src_port=>socket.localport,:dest_addr=>socket.peerhost,:dest_port=>socket.peerport)
+          $stdout.puts "LOG: #{caused_by} #{data.size} Bytes on #{socket}"
+        rescue
+          $stdout.puts "error: #{$!.inspect}:#{$!.backtrace}"
+       end
+      end
+
+    end# module Commands
+  end# module EvasionDB
 end# module FIDIUS
