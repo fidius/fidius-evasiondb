9a53021f62d5a98842a9f3f56a5fb837db8c84ce
bernd <B.Katzmarski@gmx.de>
2011-03-28 14:26:40 +0200

    tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,31 +1,45 @@
 require 'rubygems'
-gemfile = File.expand_path('../../Gemfile', __FILE__)
-ENV['BUNDLE_GEMFILE'] = gemfile
-require 'bundler'
-Bundler.setup(:test)
-#exit!
+require 'sqlite3'
 require 'simplecov'
-SimpleCov.start
-
-require 'test/unit'
 TEST_DIR = File.dirname(File.expand_path(__FILE__))
 LIB_DIR = File.join File.dirname(__FILE__),"..","lib"
+
+#puts "bla: #{ File.join(LIB_DIR,"fidius-evasiondb")}"
+#SimpleCov.root(File.join(LIB_DIR,"fidius-evasiondb"))
+SimpleCov.add_group 'Core', "./lib/evasion-db"
+SimpleCov.add_group 'Knowledge', "./lib/evasion-db/knowledge/*"
+SimpleCov.add_group 'Fetchers', "./lib/evasion-db/idmef-fetchers/*"
+SimpleCov.add_group 'Recorders', "./lib/evasion-db/recorders/*"
+SimpleCov.add_filter "./lib/db/migrations/"
+SimpleCov.start
+
+#gemfile = File.expand_path('../../Gemfile', __FILE__)
+#ENV['BUNDLE_GEMFILE'] = gemfile
+#require 'bundler'
+#Bundler.setup(:test)
+#Bundler.require
+#exit!
+
+require 'test/unit'
 $LOAD_PATH.unshift LIB_DIR
 require 'lib/fidius-evasiondb'
 require 'db/db-install'
+
 
 module FIDIUS
   module EvasionDB
     
     def self.prepare_tests
+      $config_file = File.join(TEST_DIR, 'config', 'database.yml')
+      $yml_config = YAML.load(File.read($config_file))
       self.prepare_test_db
-      FIDIUS::EvasionDB.config(File.join(TEST_DIR, 'config', 'database.yml'))
+      FIDIUS::EvasionDB.config($config_file)
       self.fill_db_with_values
 
       #load and run all tests form subdir
-      Dir.glob(File.join TEST_DIR, 'test_modules', '*.rb') do |rb|
-        require rb
-      end
+      #Dir.glob(File.join TEST_DIR, 'test_modules', '*.rb') do |rb|
+      #  require rb
+      #end
     end
 
     def self.prepare_test_db
@@ -36,6 +50,22 @@ module FIDIUS
 
       #delete and migrate new test db
       migrate(migrations_path, db_config_path)
+
+      #config = YAML.load_file("#{db_config_path}/database.yml")
+      self.setup_prelude_db
+      #create_database(config["ids_db"])
+    end
+
+    def self.setup_prelude_db
+      puts "BLA: #{$yml_config["ids_db"]["database"]}"
+      FIDIUS::PreludeDB::Connection.establish_connection $yml_config["ids_db"]
+      sql = File.read(File.join(TEST_DIR,"config","prelude.sql"))
+      sql.split(';').each do |sql_statement|
+        sql_statement = sql_statement.lstrip
+        if sql_statement != ""
+          FIDIUS::PreludeDB::Connection.connection.execute sql_statement
+        end
+      end
     end
 
     def self.fill_db_with_values
--- test/test_knowledge.rb
+++ test/test_knowledge.rb
@@ -1,24 +1,76 @@
 require 'helper'
-class TestExploit < Test::Unit::TestCase
+class TestKnowledge < Test::Unit::TestCase
   include FIDIUS::EvasionDB::Knowledge
 
   def test_options_hash_finder
-    Exploit.destroy_all
+    AttackModule.destroy_all
     5.times do
-      Exploit.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>7})
+      AttackModule.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>7})
     end
-    assert_equal 1, Exploit.all.size
-    assert_equal 2, ExploitOption.all.size
+    assert_equal 1, AttackModule.all.size
+    assert_equal 2, AttackOption.all.size
 
     5.times do
-      Exploit.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>5})
+      AttackModule.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>5})
     end
-    assert_equal 2, Exploit.all.size
-    assert_equal 4, ExploitOption.all.size
+    assert_equal 2, AttackModule.all.size
+    assert_equal 4, AttackOption.all.size
     5.times do
-      Exploit.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>5,:c=>9})
+      AttackModule.find_or_create_by_name_and_options("windows-exploit",{:a=>3,:b=>5,:c=>9})
     end
-    assert_equal 3, Exploit.all.size
-    assert_equal 7, ExploitOption.all.size
+    assert_equal 3, AttackModule.all.size
+    assert_equal 7, AttackOption.all.size
+  end
+
+  def test_idmef_event
+    event_payload = "hallo"
+    event = IdmefEvent.create(:text=>"testexploit",:payload=>event_payload)
+    exploit = AttackModule.create(:name=>"exploit")
+    payload = AttackPayload.create(:name=>"meterpreter")
+    event.attack_module = exploit
+    event.attack_payload = payload
+    event.save
+
+    event = IdmefEvent.find(event.id)
+    assert_equal event_payload.size, event.payload_size
+    assert_equal event_payload, event.payload
+    assert_equal exploit.id, event.attack_module.id
+    assert_equal payload.id, event.attack_payload.id
+    event = IdmefEvent.create(:text=>"testexploit")
+    assert_equal 0, event.payload_size
+  end
+
+  def test_packet
+    payload = "hallo"
+    packet = Packet.create
+    assert_equal [], packet.payload
+    packet = Packet.create(:payload=>payload)
+    assert_equal payload, packet.payload 
+  end
+
+  def test_finders
+    payload = "hallo"
+    AttackModule.destroy_all
+    Packet.destroy_all
+    IdmefEvent.destroy_all
+
+    exploit = AttackModule.create(:name=>"exploit")
+    packet = Packet.create(:payload=>payload)
+    event = IdmefEvent.create(:text=>"testexploit",:payload=>payload)
+    
+    assert_equal 1,FIDIUS::EvasionDB::Knowledge.get_exploits.size
+    assert_equal packet, FIDIUS::EvasionDB::Knowledge.get_packet(packet.id)
+
+    assert_equal 1, FIDIUS::EvasionDB::Knowledge.get_events.size
+    assert_equal event, FIDIUS::EvasionDB::Knowledge.get_event(event.id)
+    puts "#{packet.id}"
+    puts FIDIUS::EvasionDB::Knowledge.get_packet(packet.id).inspect
+    assert_equal packet, FIDIUS::EvasionDB::Knowledge.get_packet(packet.id)
+
+    assert_equal event, FIDIUS::EvasionDB::Knowledge.get_event(event.id)
+    p = FIDIUS::EvasionDB::Knowledge.get_packet_for_event(event.id)
+    assert_equal packet, p[:packet]
+    assert_equal 5, p[:length]
+    assert_equal 0, p[:index]
   end
 end
